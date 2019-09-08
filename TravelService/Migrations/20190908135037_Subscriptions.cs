913ceec240b2aa5537c6721d766fdcee828cfa50
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-09-08 15:51:01 +0200

    ef migration

---
+++ TravelService/Migrations/20190908135037_Subscriptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelService.Migrations
+{
+    public partial class Subscriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    RouteId = table.Column<string>(nullable: true),
+                    Callback = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+        }
+    }
+}
